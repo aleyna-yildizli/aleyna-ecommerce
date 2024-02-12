b7488a3464e51b7546396713bd587edb6e4fb625
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-12 21:44:43 +0300

    Installed fontawesome package And for header apply

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,5 +1,16 @@
+import data from '../data/data'
+import { faPhone, faEnvelope, faInstagram, faYoutube, faFacebook } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
+    const { phone, mail, message, socialsURL, firmName } = data.header;
+
+
     return (
-        <div>Header Layout</div>
+        <div>
+            <FontAwesomeIcon icon={faPhone} size="sm" style={{color: "#ffffff",}} />
+            <FontAwesomeIcon icon={faEnvelope} size="sm" />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faYoutube} />
+            <FontAwesomeIcon icon={faFacebook} />
+        </div>
     )
 }
