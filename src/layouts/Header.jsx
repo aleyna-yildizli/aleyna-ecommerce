d9e9ffd121bd543a0a3e7772db9bfdfe89936438
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-12 23:08:47 +0300

    Header created

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,16 +1,42 @@
-import data from '../data/data'
-import { faPhone, faEnvelope, faInstagram, faYoutube, faFacebook } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { data } from '../data/data'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+
+
 export default function Header() {
     const { phone, mail, message, socialsURL, firmName } = data.header;
 
 
     return (
         <div>
-            <FontAwesomeIcon icon={faPhone} size="sm" style={{color: "#ffffff",}} />
-            <FontAwesomeIcon icon={faEnvelope} size="sm" />
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faYoutube} />
-            <FontAwesomeIcon icon={faFacebook} />
+            <div className="bg-[#252B42] text-white flex text-center items-center justify-between px-6">
+                <div className="flex">
+                    <div className='flex items-center gap-[5px] p-2.5 '>
+                        <FontAwesomeIcon icon={faPhone} size="sm" style={{color: "#ffffff"}} />
+                        <h6 className=''>{phone}</h6>
+                    </div>
+                    <div className='flex items-center gap-[5px] p-2.5 '>
+                        <FontAwesomeIcon icon={faEnvelope} size="sm" />
+                        <span className=''>{mail}</span>
+                    </div>
+                </div>
+                <div className='p-2.5'>
+                   <h6 className=''>{message}</h6>
+                </div>
+                <div className="flex items-center justify-start gap-2.5 p-2.5">
+                    <h6 className="">Follow us:</h6>
+                    <div className="flex flex-wrap items-center justify-start gap-1">
+                    <FontAwesomeIcon icon={faInstagram} size="sm" className="p-1"/>
+                    <FontAwesomeIcon icon={faYoutube} size="sm" className="p-1"/>
+                    <FontAwesomeIcon icon={faFacebook} size="sm" className="p-1"/>
+                    <FontAwesomeIcon icon={faTwitter} size="sm" className="p-1"/>
+                    </div>
+                </div>
+            </div>
         </div>
+        
+        
     )
 }
