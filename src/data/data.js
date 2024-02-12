87eca5266c78f6b4c61fdf283b1bd255b9b9fdb3
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-12 21:16:30 +0300

    Header, Home, ProductList created and header data created.

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const data = {
+    header: {
+        phone: "(225) 555-0118",
+        mail: "michelle.rivera@example.com",
+        message: "Follow Us and get a chance to win 80% off",
+        socialsURL: { 
+            instagram: "instagram.com", 
+            youtube: "youtube.com", 
+            facebook: "facebook.com", 
+            twitter: "twitter.com" 
+        },
+        firmName: "Brandname",
+    },
+}
