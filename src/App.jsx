e53f40f8c1f8ee1d2bc92d43854be733959dd809
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-13 23:40:25 +0300

    Css edited

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className='w-full'>
       <Header /> 
       <Switch>
         <Route path="/" exact>
--- src/components/home/HeroCarousel.jsx
+++ src/components/home/HeroCarousel.jsx
@@ -34,11 +34,13 @@ export default function HeroCarousel(props) {
                 <div className='w-full'>
                     <img  className='w-full h-screen object-cover' src={item.src} alt={item.altText}/>
                 </div>
-                <div className='flex flex-col my-9 ml-[15%] absolute text-left items-left'>
+                <div className='flex flex-col gap-10 my-9 ml-[15%] absolute text-left items-left'>
                     <h5 className='text-[16px] font-bold text-white text-left '>{item.h5}</h5>
                     <h1 className='text-[58px] font-bold text-white leading-[80px]'>{item.h1}</h1>
+                    <div>
                     <h4 className='text-[20px] font-weight text-[#FAFAFA]'>{item.h4}</h4>
                     <h6 className='text-[20px] font-weight text-[#FAFAFA]'>{item.h6}</h6>
+                    </div>
                     <button className='text-[24px] font-bold text-white bg-[#2DC071] rounded-[5px] py-3 px-2 w-[50%]'>SHOP NOW</button>
                 </div>
                 <div></div>
