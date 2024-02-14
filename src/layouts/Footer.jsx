31a4472662ff76bd0e9c3f9e6b038809cc662abf
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-14 15:04:44 +0300

    Footer layout to FontAwesome add

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
     return (
-    <div  className="mx-auto">
-        Footer!
+    <div className="">
+        <div>
+            <div className="flex justify-between items-center py-10">
+                <h3 className="text-[#252B42] text-2xl font-bold">Bandage</h3>
+                <div className="text-[#23A6F0] flex gap-[20px]">
+                    <FontAwesomeIcon icon={faFacebook} size="lg" className="p-1"/>
+                    <FontAwesomeIcon icon={faInstagram} size="lg" className="p-1"/>
+                    <FontAwesomeIcon icon={faTwitter} size="lg" className="p-1"/>
+                </div>
+                <hr className="border border-[#E6E6E6]"></hr>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
     </div>
     )
 }
