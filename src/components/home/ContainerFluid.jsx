39ed45e87b71f2431e42918667b4048c3e82c08b
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-15 17:18:54 +0300

    Created container fluid for home page

---
+++ src/components/home/ContainerFluid.jsx
@@ -0,0 +1,23 @@
+export default function ContainerFluid(props) {
+    const { h5, h1, h4, src } = props.data;
+
+    return (
+        <div className="max-w-[1440px] m-auto bg-slate-500">
+            <div>
+                <div>
+                    <img></img>
+                </div>
+                <div>
+                    <h5></h5>
+                    <h2></h2>
+                    <h4></h4>
+                    <div>
+                        <button></button>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
+import { data } from '../data/data'
 import HeroCarousel from "../components/home/HeroCarousel"
 import BottomCarousel from "../components/home/BottomCarousel"
-import { data } from '../data/data'
+import ContainerFluid from "../components/home/ContainerFluid"
 
 export default function Home() {
     return (
-    <div  className="">
-    <div>
-        <HeroCarousel data={data.home.heroWomen}/>
-        <BottomCarousel data={data.home.heroMan}/>
-    </div>
-    </div>
+        <div className="">
+            <div>
+                <HeroCarousel data={data.home.heroWomen} />
+                <BottomCarousel data={data.home.heroMan} />
+                <ContainerFluid data={data.home.containerFluid} />
+            </div>
+        </div>
     )
 }
