b1b18946a783c6c637f66ff210766994b812de59
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-17 22:18:30 +0300

    Featured Posts created

--- src/components/home/FeaturedPosts.jsx
+++ src/components/home/FeaturedPosts.jsx
@@ -1,7 +1,18 @@
-export default function FeaturedPosts() {
-    return (
-        <div>
+import { data } from '../../data/data';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+export default function FeaturedPosts(props) {
+    const { featuredPostsText, featuredPosts, featuredPostsLinks } = props.data;
+    return (
+        <div className=" flex flex-col justify-center items-center py-28 gap-20">
+            <div className='flex flex-col text-center gap-3'>
+                <h6 className="text-[#23A6F0] text-sm font-bold">{featuredPostsText.h6}</h6>
+                <h3 className="text-[#252B42] text-[40px] font-bold">{featuredPostsText.h3}</h3>
+                <p className="text-[#737373] text-sm font-normal">{featuredPostsText.p}</p>
+            </div>
+            <div>
+                mapping
+            </div>
         </div>
     )
 }
