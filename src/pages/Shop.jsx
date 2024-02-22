95a2ac53630429a7a7af72a1d078e02223e76563
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-22 09:51:55 +0300

    FontAwesome icon added

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight, faBorderAll, faListCheck } from "@fortawesome/free-solid-svg-icons";
 import ProductCard from "../components/shop/ProductCard";
 import Paginations from "../components/shop/Paginations";
 import CategoryCard from "../components/shop/CategoryCard";
@@ -18,7 +18,19 @@ export default function Shop() {
                         <p className="text-[#BDBDBD] text-[14px] font-bold">Shop</p>
                     </div>
                 </div>
-                <CategoryCard />
+                {/* Category Card Componenti Buraya */}
+                <div className="flex justify-between py-[36px] items-center w-[65%] mx-auto">
+                    <p className="text-[#737373] font-bold">Showing all 12 results</p>
+                    <div className="flex items-center gap-3">
+                        <p className="text-[#737373] text-sm font-bold">Views:</p>
+                        <FontAwesomeIcon icon={faBorderAll} className="p-2 border rounded cursor-pointer mb-3" />
+                        <FontAwesomeIcon icon={faListCheck} className="p-2 border rounded cursor-pointer mb-3" />
+                    </div>
+                    <div className="flex items-center gap-[15px]">
+                        <div className="border rounded px-[20px] py-[10px] text-sm text-[#737373] bg-[#F9F9F9]">Popularity</div>
+                        <button className="bg-[#23A6F0] px-[20px] py-[10px] text-white text-sm font-bold rounded">Filter</button>
+                    </div>
+                </div>
                 <ProductCard />
                 <Paginations />
                 <Clients />
