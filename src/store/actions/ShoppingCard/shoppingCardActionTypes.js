4b0d6418a656eb4b673bc93489fb3aa6e8de4759
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-28 19:27:01 +0300

    Created reducers and actions for Shopping Card

---
+++ src/store/actions/ShoppingCard/shoppingCardActionTypes.js
@@ -0,0 +1,14 @@
+// SEPETTEKİ ÜRÜNLER İÇİN EYLEM TÜRLERİ
+export const ADD_TO_CART = 'ADD_TO_CART'; // Sepete ürün eklemek için eylem türü
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'; // Sepetten ürün çıkarmak için eylem türü
+export const CLEAR_CART = 'CLEAR_CART'; // Sepeti temizlemek için eylem türü
+export const UPDATE_CART_ITEM_QUANTITY = "UPDATE_CART_ITEM_QUANTITY"; // Sepet öğe miktarını güncellemek için eylem türü
+
+//ÖDEME BİLGİLERİ İÇİN EYLEM TÜRLERİ
+export const SET_PAYMENT_INFO = 'SET_PAYMENT_INFO'; // Ödeme bilgilerini ayarlamak için eylem türü
+export const SET_CHECK_STATUS = "SET_CHECK_STATUS"; // Ödeme işleminin durumununu ayarlamak için eylem türü
+export const UPDATE_PAYMENT_INFO = 'UPDATE_PAYMENT_INFO'; // Ödeme bilgilerini güncellemek için eylem türü
+
+//ADRES BİLGİLERİ İÇİN EYLEM TÜRLERİ
+export const SET_ADDRESS_INFO = 'SET_ADDRESS_INFO'; // Adres bilgilerini ayarlamak için eylem türü
+export const ADD_TO_ADDRESSES = 'ADD_TO_ADDRESSES'; // Adreslere ekleme yapmak için eylem türü
