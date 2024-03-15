0aaf5a4758183ac2dc38638c18929fc16c7ad91c
aleyna-yildizli <aleynayildizli@gmail.com>
2024-03-15 05:58:21 +0300

    Loading spinner changed AND page for empty category page created

--- src/pages/EmptyPage.jsx
+++ src/pages/EmptyPage.jsx
@@ -1,9 +1,13 @@
+import { PiPiggyBankDuotone } from "react-icons/pi";
+
 export default function EmptyPage() {
   return (
-    <div className="flex text-center p-[60px] text-[55px] font-semibold  text-slate-800 leading-2 -tracking-tighter">
+    <div className="flex text-center text-[50px] font-bold  text-slate-800 -tracking-tight py-[70px]">
       <p className="">
         For All Your Needs, There's Only One Place:{" "}
         <p className="text-pink-400">PiggyBank!</p>
+        <p className="text-lg">Sell on PiggyBank.</p>
+        <PiPiggyBankDuotone className=" text-pink-400 text-[50px] ml-[480px]" />
       </p>
     </div>
   );
